38e7509c5c311d948cb0f09c47463eff48aa114e
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-04-26 11:37:51 -1000

    add a new song function is complete in the controller

--- www/js/controllers/song-Controller.js
+++ www/js/controllers/song-Controller.js
@@ -3,22 +3,18 @@ angular.module('app.songController', [])
 .controller('songCtrl', function($scope, songService) {
   $scope.Songs = [];
 
- console.log('11111 the controller works');
-
   $scope.getAllSongs = function(){
-    console.log('222222222');
     songService.getAllSongs()
     .success(function(data){
-      console.log(66666666);
       $scope.Songs = data;
     });
   };
 
-  $scope.addSong = function(){
-    console.log('3333333333');
-    songService.addSong()
+  $scope.addSong = function(song){
+    console.log('11111111',song);
+    songService.addSong(song)
     .success(function(data){
-      console.log('6666666666');
+      console.log(55555555 , data);
 
     });
   };
