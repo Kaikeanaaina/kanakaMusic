167f17ef0df4be903bdf3bf364d27b336e059537
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-04-26 00:10:06 -1000

    added $http request to get all songs

--- www/js/services/services.js
+++ www/js/services/services.js
@@ -1,10 +1,18 @@
-angular.module('app.services', [])
+angular.module('app.songServices', [])
 
-.factory('BlankFactory', [function(){
+.service('songService', ['$http',function($http){
+  var isCordovaApp = document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1;
+  var domain;
+  if(!isCordovaApp){
+    domain = 'http://localhost:4000';
+  }
+  if(isCordovaApp){
+    domain = '.';
+  }
 
-}])
-
-.service('BlankService', [function(){
-
+  this.getAllSongs = function(){
+    console.log(333333333);
+    return $http.get(domain + '/songs');
+  };
 }]);
 
