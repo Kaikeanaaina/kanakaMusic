a77c5514d2f0e1c9c4ec3bf680e25536870067b3
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-04-26 00:09:13 -1000

    added the songService module, getAllSongs() now in scope

--- www/js/controllers/song-Controller.js
+++ www/js/controllers/song-Controller.js
@@ -1,5 +1,15 @@
 angular.module('app.songController', [])
 
-.controller('songCtrl', function($scope) {
+.controller('songCtrl', function($scope, songService) {
+  $scope.Songs = [];
+
  console.log('11111 the controller works');
+ $scope.getAllSongs = function(){
+  console.log('222222222');
+  songService.getAllSongs()
+  .success(function(data){
+    console.log(66666666);
+    $scope.Songs = data;
+  });
+ };
 });
