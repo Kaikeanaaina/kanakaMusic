32e439214641cefea0641692ed8710c7cd518b04
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-04-26 11:01:36 -1000

    adding the add song function in the controller

--- www/js/controllers/song-Controller.js
+++ www/js/controllers/song-Controller.js
@@ -4,12 +4,23 @@ angular.module('app.songController', [])
   $scope.Songs = [];
 
  console.log('11111 the controller works');
- $scope.getAllSongs = function(){
-  console.log('222222222');
-  songService.getAllSongs()
-  .success(function(data){
-    console.log(66666666);
-    $scope.Songs = data;
-  });
- };
+
+  $scope.getAllSongs = function(){
+    console.log('222222222');
+    songService.getAllSongs()
+    .success(function(data){
+      console.log(66666666);
+      $scope.Songs = data;
+    });
+  };
+
+  $scope.addSong = function(){
+    console.log('3333333333');
+    songService.addSong()
+    .success(function(data){
+      console.log('6666666666');
+
+    });
+  };
+
 });
