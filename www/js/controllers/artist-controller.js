9f31412aa9e610c3c68d4d7caa2ac2e6820c75ab
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-05-07 20:31:21 -1000

    added the empty arrays for the artists

--- www/js/controllers/artist-controller.js
+++ www/js/controllers/artist-controller.js
@@ -3,5 +3,36 @@ angular.module('app.artistController', [])
 
 
 .controller('artistCtrl', function($scope) {
+
+  $scope.Artist = [];
+  $scope.NumberArtist = [];
+  $scope.AArtist = [];
+  $scope.BArtist = [];
+  $scope.CArtist = [];
+  $scope.DArtist = [];
+  $scope.EArtist = [];
+  $scope.FArtist = [];
+  $scope.GArtist = [];
+  $scope.HArtist = [];
+  $scope.IArtist = [];
+  $scope.JArtist = [];
+  $scope.KArtist = [];
+  $scope.LArtist = [];
+  $scope.MArtist = [];
+  $scope.NArtist = [];
+  $scope.OArtist = [];
+  $scope.PArtist = [];
+  $scope.QArtist = [];
+  $scope.RArtist = [];
+  $scope.SArtist = [];
+  $scope.TArtist = [];
+  $scope.UArtist = [];
+  $scope.VArtist = [];
+  $scope.WArtist = [];
+  $scope.XArtist = [];
+  $scope.YArtist = [];
+  $scope.ZArtist = [];
+
+
 console.log('this is the artist controller');
 });
