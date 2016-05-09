fc3428816a14bbc1fdd7d6e5fd997355b69bbeed
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-05-09 02:02:30 -1000

    the start of the artist-controller

--- www/js/controllers/artist-controller.js
+++ www/js/controllers/artist-controller.js
@@ -2,10 +2,10 @@
 angular.module('app.artistController', [])
 
 
-.controller('artistCtrl', function($scope) {
+.controller('artistCtrl', function($scope, $location, artistService) {
 
-  $scope.Artist = [];
   $scope.NumberArtist = [];
+  $scope.Artist = [];
   $scope.AArtist = [];
   $scope.BArtist = [];
   $scope.CArtist = [];
@@ -33,6 +33,72 @@ angular.module('app.artistController', [])
   $scope.YArtist = [];
   $scope.ZArtist = [];
 
+  $scope.getAllArtists = function(){
+    console.log('1111111');
+    artistService.getAllArtists()
+    .success(function(data){
+      console.log('555555555');
+
+      for(var i = 0;i<data.length;i++){
+        var splitTitle = data[i].title.toUpperCase().split("");
+        if(splitTitle[0]==="A"){
+          $scope.AArtist.push(data[i]);
+        } else if(splitTitle[0]==="B"){
+          $scope.BArtist.push(data[i]);
+        } else if(splitTitle[0]==="C"){
+          $scope.CArtist.push(data[i]);
+        } else if(splitTitle[0]==="D"){
+          $scope.DArtist.push(data[i]);
+        } else if(splitTitle[0]==="E"){
+          $scope.EArtist.push(data[i]);
+        } else if(splitTitle[0]==="F"){
+          $scope.FArtist.push(data[i]);
+        } else if(splitTitle[0]==="G"){
+          $scope.GArtist.push(data[i]);
+        } else if(splitTitle[0]==="H"){
+          $scope.HArtist.push(data[i]);
+        } else if(splitTitle[0]==="I"){
+          $scope.IArtist.push(data[i]);
+        } else if(splitTitle[0]==="J"){
+          $scope.JArtist.push(data[i]);
+        } else if(splitTitle[0]==="K"){
+          $scope.KArtist.push(data[i]);
+        } else if(splitTitle[0]==="L"){
+          $scope.MArtist.push(data[i]);
+        } else if(splitTitle[0]==="M"){
+          $scope.MArtist.push(data[i]);
+        } else if(splitTitle[0]==="N"){
+          $scope.NArtist.push(data[i]);
+        } else if(splitTitle[0]==="O"){
+          $scope.OArtist.push(data[i]);
+        } else if(splitTitle[0]==="P"){
+          $scope.PArtist.push(data[i]);
+        } else if(splitTitle[0]==="Q"){
+          $scope.QArtist.push(data[i]);
+        } else if(splitTitle[0]==="R"){
+          $scope.RArtist.push(data[i]);
+        } else if(splitTitle[0]==="S"){
+          $scope.SArtist.push(data[i]);
+        } else if(splitTitle[0]==="T"){
+          $scope.TArtist.push(data[i]);
+        } else if(splitTitle[0]==="U"){
+          $scope.UArtist.push(data[i]);
+        } else if(splitTitle[0]==="V"){
+          $scope.VArtist.push(data[i]);
+        } else if(splitTitle[0]==="W"){
+          $scope.WArtist.push(data[i]);
+        } else if(splitTitle[0]==="X"){
+          $scope.XArtist.push(data[i]);
+        } else if(splitTitle[0]==="Y"){
+          $scope.YArtist.push(data[i]);
+        } else if(splitTitle[0]==="Z"){
+          $scope.ZArtist.push(data[i]);
+        } else {
+          $scope.NumberArtist.push(data[i]);
+        }
+      }
+    });
+  };
 
 console.log('this is the artist controller');
 });
