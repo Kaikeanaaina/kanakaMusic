d48f97db5b599e393d677cd2f3688e39905c7e89
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-05-09 19:41:55 -1000

    took out the urlName, allow description to be null

--- models/Artist.js
+++ models/Artist.js
@@ -4,13 +4,9 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING( 255 ),
       allowNull: false
     },
-    urlName : {
-      type: DataTypes.STRING( 255 ),
-      allowNull: false
-    },
     description : {
       type: DataTypes.STRING( 255 ),
-      allowNull: false
+      allowNull: true
     },
     }, {
     classMethods : {
