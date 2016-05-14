209e3c40534a98b263dca0d53c9cd483ae78d121
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-05-15 07:17:56 +1000

    added the record label controller with the many arrays

--- www/js/controllers/recordLabel-Controller.js
+++ www/js/controllers/recordLabel-Controller.js
@@ -3,4 +3,35 @@ angular.module('app.recordLabelController', [])
 
 .controller('recordLabelCtrl', function($scope) {
 console.log('this is the record label controller');
+
+  $scope.RecordLabel = [];
+  $scope.NumberRecordLabel = [];
+  $scope.ARecordLabel = [];
+  $scope.BRecordLabel = [];
+  $scope.CRecordLabel = [];
+  $scope.DRecordLabel = [];
+  $scope.ERecordLabel = [];
+  $scope.FRecordLabel = [];
+  $scope.GRecordLabel = [];
+  $scope.HRecordLabel = [];
+  $scope.IRecordLabel = [];
+  $scope.JRecordLabel = [];
+  $scope.KRecordLabel = [];
+  $scope.LRecordLabel = [];
+  $scope.MRecordLabel = [];
+  $scope.NRecordLabel = [];
+  $scope.ORecordLabel = [];
+  $scope.PRecordLabel = [];
+  $scope.QRecordLabel = [];
+  $scope.RRecordLabel = [];
+  $scope.SRecordLabel = [];
+  $scope.TRecordLabel = [];
+  $scope.URecordLabel = [];
+  $scope.VRecordLabel = [];
+  $scope.WRecordLabel = [];
+  $scope.XRecordLabel = [];
+  $scope.YRecordLabel = [];
+  $scope.ZRecordLabel = [];
+
+
 });
