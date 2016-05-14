5fbe73a052b26784cc3b7b33d867c644c822adde
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-05-15 07:14:45 +1000

    changed associations

--- models/RecordLabel.js
+++ models/RecordLabel.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods : {
       associate : function(models) {
-        RecordLabel.hasMany(models.Artist);
+        RecordLabel.hasMany(models.Album);
       }
     }
   });
