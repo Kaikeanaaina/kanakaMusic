9f5e81a614bdf2bd50c13b2c681baea06e8d27de
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-06-06 12:18:51 -1000

    changed the login area

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic','app.routes', 'app.Controllers', 'app.songServices', 'app.artistServices','app.albumServices', 'app.directives', 'app.gigController', 'app.recordLabelServices','app.settingController','app.bandMemberController','app.Filters'])
+angular.module('app', ['ionic','app.routes', 'app.Controllers', 'app.songServices', 'app.artistServices','app.albumServices', 'app.directives', 'app.gigController', 'app.recordLabelServices','app.settingController','app.bandMemberController','app.loginServices','app.Filters'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -304,7 +304,7 @@ angular.module('app', ['ionic','app.routes', 'app.Controllers', 'app.songService
     views: {
       'side-menu21': {
         templateUrl: 'templates/login.html',
-        controller: 'settingCtrl'
+        controller: 'kanakaMusicCtrl'
       }
     }
   });
