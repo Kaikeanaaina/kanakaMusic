754ab47c9688f2bfbf17220fbb439d3bbe9c8d48
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-06-20 18:52:16 -1000

    log out funciton is functionable, takes you to the log in after you log out

--- www/js/controllers/setting-Controller.js
+++ www/js/controllers/setting-Controller.js
@@ -1,5 +1,12 @@
 angular.module('app.settingController', [])
 
-.controller('settingCtrl', function($scope) {
+.controller('settingCtrl', function($scope, $state, loginService) {
 console.log('this is the settings controller');
+
+  $scope.logOut = function(){
+    loginService.logOutUser().success(function(data){
+      return $state.go('menu.login');
+    });
+  };
+
 });
