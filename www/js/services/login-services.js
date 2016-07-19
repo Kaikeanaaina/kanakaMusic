73ee6f870c6388f8383b558e5859482b68c2c05d
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-07-18 21:48:28 -1000

    removed the content of the login service for the consumer view

--- www/js/services/login-services.js
+++ www/js/services/login-services.js
@@ -10,16 +10,4 @@ angular.module('app.loginServices', [])
     domain = '.';
   }
 
-  this.registerUser = function(user){
-    return $http.post(domain + '/users/register', user);
-  };
-
-  this.loginUser = function(user){
-    console.log(2222, user);
-    return $http.post(domain + '/users/login', user);
-  };
-
-  this.logOutUser = function() {
-    return $http.get(domain + '/users/logout');
-  };
 });
