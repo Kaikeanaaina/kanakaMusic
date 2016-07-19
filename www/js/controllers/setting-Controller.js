56a5989af66ec5f246333e3d7f4dc85cdcacbc22
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-07-18 21:53:40 -1000

    removed logout for consumer view in the setting controller

--- www/js/controllers/setting-Controller.js
+++ www/js/controllers/setting-Controller.js
@@ -3,11 +3,4 @@ angular.module('app.settingController', [])
 .controller('settingCtrl', function($scope, $rootScope, $state, loginService) {
 console.log('this is the settings controller');
 
-  $scope.logOut = function(){
-    loginService.logOutUser().success(function(data){
-      $rootScope.loggedinUser = null;
-      return $state.go('menu.login');
-    });
-  };
-
 });
